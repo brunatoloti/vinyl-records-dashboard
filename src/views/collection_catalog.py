35013804191c7e0392328e95fcadec172a281810
Bruna Toloti <56737588+brunatoloti@users.noreply.github.com>
2026-06-01 18:41:57 -0300

    update collection catalog dashboard

--- src/views/collection_catalog.py
+++ src/views/collection_catalog.py
@@ -1,4 +1,7 @@
+from datetime import datetime
 import matplotlib.pyplot as plt
+import pandas as pd
+from plotly_calplot import calplot
 import plotly.express as px
 import streamlit as st
 from wordcloud import WordCloud
@@ -32,23 +35,111 @@ with tab1:
     vinyl_display(df_heritage)
 
 with tab2:
+    heritage_sel = st.session_state.get('heritage', [])
+    musician_sel = st.session_state.get('musician', [])
+    publication_year_sel = st.session_state.get('publication_year', [])
+    purchase_year_sel = st.session_state.get('purchase_year', [])
+
+    df['heritage_str'] = df['heritage'].apply(lambda x: 'Sim' if str(x) == '1.0' else 'Não')
+    
+    df_heritage_metric = df.copy()
+    if musician_sel:
+        df_heritage_metric = df_heritage_metric[df_heritage_metric['artist'].isin(musician_sel)]
+    if publication_year_sel:
+        df_heritage_metric = df_heritage_metric[df_heritage_metric['release_year'].isin(publication_year_sel)]
+    if purchase_year_sel:
+        df_heritage_metric = df_heritage_metric[df_heritage_metric['year'].isin(purchase_year_sel)]
+    heritage_options = sorted(df_heritage_metric['heritage_str'].unique())
+
+    df_musician_metric = df.copy()
+    if heritage_sel:
+        df_musician_metric = df_musician_metric[df_musician_metric['heritage_str'].isin(heritage_sel)]
+    if publication_year_sel:
+        df_musician_metric = df_musician_metric[df_musician_metric['release_year'].isin(publication_year_sel)]
+    if purchase_year_sel:
+        df_musician_metric = df_musician_metric[df_musician_metric['year'].isin(purchase_year_sel)]
+    musician_options = sorted(df_musician_metric['artist'].unique())
+
+    df_publication_year_metric = df.copy()
+    if musician_sel:
+        df_publication_year_metric = df_publication_year_metric[df_publication_year_metric['artist'].isin(musician_sel)]
+    if heritage_sel:
+        df_publication_year_metric = df_publication_year_metric[df_publication_year_metric['heritage_str'].isin(heritage_sel)]
+    if purchase_year_sel:
+        df_publication_year_metric = df_publication_year_metric[df_publication_year_metric['year'].isin(purchase_year_sel)]
+    publication_year_options = sorted(df_publication_year_metric['release_year'].unique())
+
+    df_purchase_year_metric = df.copy()
+    if musician_sel:
+        df_purchase_year_metric = df_purchase_year_metric[df_purchase_year_metric['artist'].isin(musician_sel)]
+    if publication_year_sel:
+        df_purchase_year_metric = df_purchase_year_metric[df_purchase_year_metric['release_year'].isin(publication_year_sel)]
+    if heritage_sel:
+        df_purchase_year_metric = df_purchase_year_metric[df_purchase_year_metric['heritage_str'].isin(heritage_sel)]
+    purchase_year_options = sorted(df_purchase_year_metric['year'].unique())
+
+    filters = st.columns(4)
+    with filters[0]:
+        heritage_sel = st.multiselect(
+            'Herdados', 
+            heritage_options,
+            default=heritage_sel,
+            key='heritage',
+            placeholder='Todos'
+        )
+    with filters[1]:
+        musician_sel = st.multiselect(
+            'Músicos/banda',
+            musician_options,
+            default=musician_sel,
+            key='musician',
+            placeholder='Todos'
+        )
+    with filters[2]:
+        publication_year_sel = st.multiselect(
+            'Ano de lançamento',
+            publication_year_options,
+            default=publication_year_sel,
+            key='publication_year',
+            placeholder='Todos'
+        )
+    with filters[3]:
+        purchase_year_sel = st.multiselect(
+            'Ano que entrou na coleção',
+            purchase_year_options,
+            default=purchase_year_sel,
+            key='purchase_year',
+            placeholder='Todos'
+        )
+
+    result = df.copy()
+
+    if heritage_sel:
+        result = result[result['heritage_str'].isin(heritage_sel)]
+    if musician_sel:
+        result = result[result['artist'].isin(musician_sel)]
+    if publication_year_sel:
+        result = result[result['release_year'].isin(publication_year_sel)]
+    if purchase_year_sel:
+        result = result[result['year'].isin(purchase_year_sel)]
+
     col_indicator = st.columns(5)
     with col_indicator[0]:
-        st.metric(label='📀 Quantidade de discos', value=df.shape[0])
+        st.metric(label='📀 Quantidade de discos', value=result.shape[0])
     with col_indicator[1]:
-        st.metric(label='🧑‍🎤 Quantidade de músicos/bandas', value=df.drop_duplicates(subset=['artist']).shape[0])
+        st.metric(label='🧑‍🎤 Quantidade de músicos/bandas', value=result.drop_duplicates(subset=['artist']).shape[0])
     with col_indicator[2]:
-        st.metric(label='🖤 Quantidade de discos herdados', value=int(df.heritage.sum()))
+        st.metric(label='🖤 Quantidade de discos herdados', value=int(result.heritage.sum()))
     with col_indicator[3]:
-        st.metric(label='⭐ Quantidade de discos usados', value=int(df.used.sum()))
+        st.metric(label='⭐ Quantidade de discos usados', value=int(result.used.sum()))
     with col_indicator[4]:
-        st.metric(label='🎁 Quantidade de presentes', value=int(df.gift.sum()))
+        st.metric(label='🎁 Quantidade de presentes', value=int(result.gift.sum()))
 
     col1, col2 = st.columns([1, 1])
 
     with col1:
         # chart of number of records by country
-        records_by_country = df.groupby('country')['name'].count().reset_index()
+        records_by_country = result.groupby('country')['name'].count().reset_index()
         records_by_country = records_by_country.merge(get_all_countries(), left_on='country', right_on='value').drop(columns=['value'])
         records_by_country = records_by_country.rename(columns={'id': 'IdISO3166'})
         chart1 = px.scatter_geo(records_by_country, locations='IdISO3166',
@@ -80,7 +171,7 @@ with tab2:
             </h6>
         """, unsafe_allow_html=True)
         st.write('')
-        counting = df['artist'].value_counts().to_dict()
+        counting = result['artist'].value_counts().to_dict()
 
         wc = WordCloud(width=800, height=400, background_color="#cfd0daff", colormap="Reds")
         wc.generate_from_frequencies(counting)
@@ -92,7 +183,7 @@ with tab2:
         
 
 
-    records_by_year = df.groupby('year')['name'].count().reset_index()
+    records_by_year = result.groupby('year')['name'].count().reset_index()
     records_by_year['year'] = records_by_year['year'].apply(lambda x: str(int(x)))
     chart2 = px.line(records_by_year, x='year', y='name', 
                     title=f'Quantidade de discos que entraram na coleção por ano', color_discrete_sequence=["#FF4B4B"], text='name')
@@ -115,7 +206,7 @@ with tab2:
     chart2.update_xaxes(title_text='')
     st.plotly_chart(chart2)
 
-    counting = df.groupby('release_year').agg(
+    counting = result.groupby('release_year').agg(
             records = ('name', lambda x: '<br>'.join(sorted(x))),
             count = ('name', 'count')
         ).reset_index()
@@ -144,7 +235,7 @@ with tab2:
     col1, col2 = st.columns([1, 1])
 
     with col1:
-        count_gift_person = df.groupby('gift_person')['name'].count().reset_index()
+        count_gift_person = result.groupby('gift_person')['name'].count().reset_index()
         count_gift_person = count_gift_person.sort_values('name', ascending=False).head()
         chart4 = px.bar(count_gift_person, x='name', y="gift_person", orientation='h',
                     height=400,
@@ -164,7 +255,7 @@ with tab2:
         chart4.update_xaxes(title_text='')
         st.plotly_chart(chart4)
 
-        count_finished_books_by_new_reading = df['purchase_type'].value_counts().reset_index()
+        count_finished_books_by_new_reading = result['purchase_type'].value_counts().reset_index()
         count_finished_books_by_new_reading.columns = ['purchase_type', 'Qtd']
         chart7 = px.bar(count_finished_books_by_new_reading, x='purchase_type', y="Qtd", orientation='v',
                         height=400,
@@ -183,7 +274,7 @@ with tab2:
 
     with col2:
         chart5 = px.histogram(
-            df,
+            result,
             x='duration',
             title='Distribuição da duração dos álbuns',
             color_discrete_sequence=["#CF7C7C"]
@@ -193,7 +284,7 @@ with tab2:
         st.plotly_chart(chart5)
 
 
-        count_shopping_store = df.groupby('shopping_store')['name'].count().reset_index()
+        count_shopping_store = result.groupby('shopping_store')['name'].count().reset_index()
         count_shopping_store = count_shopping_store.sort_values('name', ascending=False).head()
         chart6 = px.bar(count_shopping_store, x='name', y="shopping_store", orientation='h',
                     height=400,
@@ -212,3 +303,39 @@ with tab2:
         chart6.update_yaxes(title_text='')
         chart6.update_xaxes(title_text='')
         st.plotly_chart(chart6)
+
+    entry_by_date = result[['name', 'entry_date', 'year']].sort_values('year')
+    entry_by_date = entry_by_date.groupby(['year', 'entry_date']).agg(
+        records = ('name', lambda x: '<br>'.join(sorted(x))),
+        QtDiscos = ('name', 'count')
+    ).reset_index(drop=False)
+    entry_by_date['entry_date'] = pd.to_datetime(entry_by_date['entry_date'], format='%d/%m/%Y').dt.date
+    entry_by_date['year'] = pd.to_datetime(entry_by_date['year']).dt.year
+    entry_by_date = entry_by_date.set_index('entry_date')
+    entry_by_date = entry_by_date.reindex(pd.date_range(f'01/01/{entry_by_date.index.min().year}',
+                                                        datetime.today().strftime('%m/%d/%Y')))
+    entry_by_date = entry_by_date.fillna({
+        'QtDiscos': 0,
+        'records': ''
+    })
+    entry_by_date = entry_by_date.reset_index(drop=False).rename(columns={'index': 'entry_date'})
+    chart9 = calplot(entry_by_date, x='entry_date', y='QtDiscos',
+                     cmap_min=0, cmap_max=5, name='Quantidade', colorscale='reds')
+    for trace in chart9.data:
+        if trace.type == "heatmap":
+            trace.hovertemplate = (
+                "<b>Data:</b> %{customdata[0]}<br>"
+                "<b>Quantidade:</b> %{z}"
+                "<extra></extra>"
+            )
+    dd = {'title': {'text': 'Histórico de aquisição de discos'}}
+    j = 1
+    years_df = result.sort_values('year')['year'].astype('int').unique()
+    all_years = list(range(min(years_df), max(years_df) + 1))
+    for i in all_years:
+        dd.update({f'yaxis{j}': {'title': str(i), 'ticktext': ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom']},
+                   f'xaxis{j}': {'ticktext': ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho',
+                                              'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']}})
+        j = j + 1
+    chart9.update_layout(dd)
+    st.plotly_chart(chart9)
